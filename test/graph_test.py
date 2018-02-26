41f8207db376a489ebf9c33a199954b7b843e66b
Xiaoyan Wang <horizonblueblueblue@gmail.com>
2018-02-26 05:32:02 -0600

    Setup unittest

---
+++ test/graph_test.py
@@ -0,0 +1,52 @@
+from unittest import TestCase
+from model.graph import Graph
+
+
+class TestGraph(TestCase):
+    def setUp(self):
+        self.graph = Graph()
+
+    # def test_add(self):
+    #     self.fail()
+    #
+    # def test_add_actor(self):
+    #     self.fail()
+    #
+    # def test_add_movie(self):
+    #     self.fail()
+    #
+    # def test_load(self):
+    #     self.fail()
+    #
+    # def test_dump(self):
+    #     self.fail()
+    #
+    # def test_get_movie(self):
+    #     self.fail()
+    #
+    # def test_get_actor(self):
+    #     self.fail()
+    #
+    # def test_get_gross_income(self):
+    #     self.fail()
+    #
+    # def test_get_movies(self):
+    #     self.fail()
+    #
+    # def test_get_actors(self):
+    #     self.fail()
+    #
+    # def test_get_actor_rank(self):
+    #     self.fail()
+    #
+    # def test_get_oldest_actors(self):
+    #     self.fail()
+    #
+    # def test_get_movies_by_year(self):
+    #     self.fail()
+    #
+    # def test_get_actors_by_year(self):
+    #     self.fail()
+    #
+    # def test_get_counts(self):
+    #     self.fail()
